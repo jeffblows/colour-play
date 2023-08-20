9dedc560875f5bbc3f7b57b52d158ea49a2249d3
jeff-at-615871325786 <jeff@jikkentechnology>
2023-08-20 15:45:10 +1000

    add seconds

--- src/main.c
+++ src/main.c
@@ -28,19 +28,24 @@ int main(int argc, char** argv) {
 
   initscr();
   start_color();
-
   init_pair(1, COLOR_RED, COLOR_BLACK);
-  attron(COLOR_PAIR(1));
+
   uint8_t i = 0;
+  mvwprintw(stdscr, 0, 0, "HW %09d\r", i);
+  attron(COLOR_PAIR(1));
   while (!exit_program) {
-    mvwprintw(stdscr, 0, 0, "HW %09d\r", i);
+    mvwprintw(stdscr, 0, 3, "%09d\r", i);
     refresh();
-//    fflush(stdout);
     usleep(100000);
     i++;
+    if (i % 10 == 0) {
+      attroff(COLOR_PAIR(1));
+      mvwprintw(stdscr, LINES - 1, 0, "%09d\r", i);
+      attron(COLOR_PAIR(1));
+    }
   }
   attroff(COLOR_PAIR(1));
-  printf("\nexiting now\n");
   endwin();
+  printf("\nexiting now\n");
 }
 
