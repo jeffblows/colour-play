4866ab62774ed9b4696b66119c5d286bdd871200
Jeff Blows <jeff@jikkentechnology.com>
2023-08-29 07:29:28 +1000

    improve command error messages

--- src/process_command_line.c
+++ src/process_command_line.c
@@ -10,7 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <errno.h>
+#include <stdbool.h>
 
 #include "process_command_line.h"
 
@@ -22,16 +23,26 @@ command_line_params_struct command_line_params = {.verbose = 0, .loop_count = 0,
  *
  */
 void show_usage(char * command_name) {
-  fprintf(stderr, "Usage: %s [-v verbose_level] <loop_count>\n", command_name);
+  fprintf(stderr, "Usage: %s [-v verbose_level] [-p] <loop_count>\n", command_name);
+  fprintf(stderr, "\t-v <n> verbose error message level\n");
+  fprintf(stderr, "\t-p thread model to use. Default is C11 threads. Option is pthreads.\n");
 }
 
 int process_command_line(int argc, char** argv) {
   int opt;
+  bool usage_error = false;
 
   while ((opt = getopt(argc, argv, "pv:")) != -1) {
     switch (opt) {
       case 'v': {
-        command_line_params.verbose = strtoul(optarg, NULL, 0);
+        long int lv = strtol(optarg, NULL, 0);
+        if ((errno == ERANGE) || (errno == EINVAL)) {
+          show_usage(argv[0]);
+          fprintf(stderr, "Verbose option invalid:\n");
+          usage_error = true;
+          continue;
+        }
+        command_line_params.verbose = lv % MAX_VERBOSE_LEVEL;
         break;
       }
       case 'p': {
@@ -40,9 +51,12 @@ int process_command_line(int argc, char** argv) {
       }
       default: {
         show_usage(argv[0]);
-        return PCL_BAD_OPTION;
+        usage_error = true;
       }
     }
+  }
+  if (usage_error) {
+    return PCL_BAD_OPTION;
   }
   if (optind >= argc) {
     // we have run out of parameters without getting our counter
